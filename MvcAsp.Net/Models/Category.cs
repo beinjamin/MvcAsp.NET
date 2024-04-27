869d00494cbcb257de6f08fe7ffb5169bc240bb0
beinjamin <jauresbeinjamin@gmail.com>
2024-04-27 19:33:56 +0100

    Mise en place view lie model

--- MvcAsp.Net/Models/Category.cs
+++ MvcAsp.Net/Models/Category.cs
@@ -3,7 +3,7 @@ namespace MvcAsp.Net.Models;
 public class Category
 {
     public int CategoryId { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
 }
---
+++ MvcAsp.Net/Views/Categories/Edit.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcAsp.Net.Views.Categories;
+
+public class Edit : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
